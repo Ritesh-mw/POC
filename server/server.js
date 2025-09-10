df787e9553b9eaa26265ff6467323027f836d01b
kunuguntla ritesh <kunuguntla.ritesh@magicworkshop.ai>
2025-09-10 11:52:12 +0530

    updated for cors error

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+    origin: "https://papaya-choux-8425ed.netlify.app", // your Netlify URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use('/api', authRoutes);
 
 app.get('/', (_req, res) => {
